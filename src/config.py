d87e090d665e68f2e92373cdeb46b1071eb7f421
Scott Sikorski <sgs.sikorski20@gmail.com>
2025-03-18 20:23:12 -0400

    Some LLM policy additions

--- src/config.py
+++ src/config.py
@@ -7,3 +7,10 @@ MAX_STEERING = 1
 MIN_ACCELERATION = -1
 MIN_STEERING = -1
 LANE_OFFSET = 2
+
+from enum import Enum, auto
+
+
+class Role(Enum):
+    LEADER = auto()
+    FOLLOWER = auto()
--- src/policies/LLMPolicy.py
+++ src/policies/LLMPolicy.py
@@ -7,15 +7,17 @@
 
 import openai
 import os
+from config import Role
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 
 class LLMPolicy:
-    def __init__():
+    def __init__(self, location):
         # TODO: Possibly modify this
-        content = """
-There is another agent taking someone to a location. 
+        content = f"""
+You are going to {location}.
+There is another agent going to a location. 
 You will have a conversation until you determine whether you have more, less, or the same priority as them depending on the task you and they are performing. 
 If you are doing the same task or have the same priority then say so. 
 Do not include pleasantries and be concise. 
@@ -23,6 +25,7 @@ Once you have reached a consensus with the other agent, output the number 1 and 
 Remembering your task correctly is paramount!
 """.strip()
         initial_msg = {"role": "system", "content": content}
+        self.messages = [initial_msg]
 
     def query(self, role, prompt, persist=True):
         new_msg = {"role": role, "content": prompt}
@@ -36,3 +39,25 @@ Remembering your task correctly is paramount!
             self.messages.append(completion.choices[0].message)
 
         return completion.choices[0].message.content
+
+    def get_role(self):
+        prompt = """
+You have come to a consensus. 
+It is vital you remember what you agreed on with the other agent! 
+If your task is more important than the other agent's task, output the number 2. 
+If your task is less important than the other agent's task, output the number 3.
+""".strip()
+
+        code = self.query("system", prompt, persist=False)
+
+        if "2" in code:
+            return Role.LEADER
+        if "3" in code:
+            return Role.FOLLOWER
+        if "4" in code:
+            return None
+
+    def get_leader_followers(self, agents):
+        raise NotImplementedError(
+            "get_leader_followers for the designated policy is not implemented yet"
+        )
